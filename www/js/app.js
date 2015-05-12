212da5bb081f896d7c32aa698825e0e7c54c6aee
Kristian Lundström <kristian.lundstrom@gmail.com>
2015-05-12 12:10:46 +0200

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,44 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'timer', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  .state('index', {
+    url: '/',
+        templateUrl: 'templates/firstpage.html',
+        controller: 'FirstpageController'
+  })
+
+    .state('score', {
+    url: '/score',
+        templateUrl: 'templates/score.html',
+        controller: 'ScoreController'
+  });
+
+   $urlRouterProvider.otherwise('/');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('starter.controllers', [])
+
+.controller('FirstpageController', function($scope) {
+	console.log("We");
+	$scope.timerRunning = false;
+	$scope.workMessage = "Time to start working!";
+	$scope.buttonText = "Start session";
+	$scope.startTimer = function() {
+		console.log("start"); 
+		$scope.$broadcast('timer-start');
+		$scope.timerRunning = true;
+		$scope.workMessage = "Keep working for";
+		$scope.buttonText = "Session active";
+
+		$scope.style = {
+			color: 'red'
+		};
+
+	};
+
+
+	$scope.$on('timer-stopped', function (event, data){
+		console.log('Timer Stopped - data = ', data);
+		$scope.timerRunning = false;
+		$scope.workMessage = "Session completed";
+		$scope.style = {
+			color: 'green'
+		};
+	});
+})
+
+.controller('ScoreController', function($scope) {
+	$scope.items = ["A", "List", "Of", "Items"];
+});
