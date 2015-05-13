717dc609a38d9406bf17bb30dc2953c3fb2db564
User <alex@eduroam-94-21.public.su.se>
2015-05-13 16:04:41 +0200

    fixat så knapp byter färg och ny text kommer upp

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,30 +1,53 @@
 angular.module('starter.controllers', [])
 
-.controller('FirstpageController', function($scope) {
+.controller('FirstpageController', function($scope, $timeout) {
 	console.log("We");
 	$scope.timerRunning = false;
 	$scope.workMessage = "Time to start working!";
 	$scope.buttonText = "Start session";
 	$scope.buttonStyle = "button-positive"
 
+
+//called when timer is started (from clicking activity button)
 	$scope.startTimer = function() {
 		console.log("start"); 
 		$scope.$broadcast('timer-start');
 		$scope.timerRunning = true;
 		$scope.workMessage = "Keep working for";
 		$scope.buttonText = "Session active";
+		$scope.buttonStyle = "button-energized";
+	};
+
+//called when the timer is stopped manually, i.e session failed
+	$scope.manualStopTimer = function (){
+		$scope.$broadcast('timer-stop');
+		$scope.timerRunning = false;
+		$scope.workMessage = "You failed your session!";
+		$scope.buttonText = "Start again";
 		$scope.buttonStyle = "button-assertive";
 	};
 
+//When the activity button i clicked this checks if theres time left
+	$scope.activityButtonClicked = function(){
+		if ($scope.timerRunning) {
+			$scope.manualStopTimer();
+		}else{
+			$scope.startTimer();
+		}
+	};
 
+//called when time has run out
 	$scope.$on('timer-stopped', function (event, data){
-		console.log('Timer Stopped - data = ', data);
+		
 		$scope.timerRunning = false;
-		$scope.workMessage = "Session completed";
-		$scope.style = {
-			color: 'green'
-		};
+		if (data.seconds===0){
+			$scope.buttonStyle = "button-balanced";
+			$scope.workMessage = "Congratulations, you made it :)";
+			$scope.buttonText = "Start again";
+			$scope.$apply();
+		}
 	});
+	
 })
 
 .controller('ScoreController', function($scope) {
