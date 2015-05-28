800d3951f3ecd3fe888ba80514d0e238a366e0d8
User <alex@Users-MacBook-Pro.local>
2015-05-28 14:04:06 +0200

    ny fil med filter

---
+++ www/js/filters.js
@@ -0,0 +1,49 @@
+angular.module('starter.filters', [])
+
+.filter('levelCheck', function () {
+	return function (points) {
+
+		if (points >=0 && points <51)
+			level = 1;
+		else if (points >=51 && points <100)
+			level = 2;
+		else if (points >=100 && points <150)
+			level = 3;
+		else if (points >=150 && points<250)
+			level = 4;
+		else if (points >=250)
+			level = 5;
+
+		return level;
+	};
+});
+
+/*.filter('pointsIntervalAchieved', function () {
+	return function (item) {
+		$scope.pointsInterval = 0;
+
+		if ($scope.points >=20 && <50)
+			$scope.pointsInterval = 20;
+		else if ($scope.points >=50 && <100)
+			$scope.pointsInterval = 50;
+		else if ($scope.points >=100 && <150)
+			$scope.pointsInterval = 100;
+		else if ($scope.points >=150 && <250)
+			$scope.pointsInterval = 150;
+		else if ($scope.points >=250 && <300)
+			$$scope.pointsInterval = 250;
+		else if ($scope.points >=300 && 350<)
+			$scope.pointsInterval = 300;
+		else if ($scope.points >=350 && <400)
+			$scope.pointsInterval = 350;
+		else if ($scope.points >=400)
+			$scope.pointsInterval = 400;
+
+		return $scope.pointsInterval;
+
+		//Fungerar inte eftersom vi måste veta om användare precis tagit sig över poänggränsen
+		//Så att detta inte sker varje gång
+	};
+});
+*/
+
