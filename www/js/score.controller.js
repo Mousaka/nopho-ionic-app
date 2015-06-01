47175841f0a0ebbcb3b54a3b8590cddd3f28ead4
Kristian Lundström <kristian.lundstrom@gmail.com>
2015-06-01 17:36:36 +0200

    made angular-circular-progress work

--- www/js/score.controller.js
+++ www/js/score.controller.js
@@ -1,19 +1,15 @@
 (function () {
 
-function scorePageController($ionicActionSheet, $ionicPlatform, $scope){
-	this.points = 0;
-	this.level = 1
-}
+	function scorePageController($ionicActionSheet, $ionicPlatform, $scope){
+		this.points = 0;
+		this.level = 1
+		$scope.roundProgress = {
+			label: 100,
+			percentage: 75
+		};
+	}
 
-angular.module('starter.score.controller', [])
-.controller('ScorePageController', scorePageController);
-
-     
-
-$scope.roundProgress = {
-  label: 100,
-  percentage: 75
-};
-
+	angular.module('starter.score.controller', [])
+	.controller('ScorePageController', scorePageController);
 
 })();
