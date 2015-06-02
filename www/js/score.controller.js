83badc5be696f7f968594583e1c9d32c04db940b
Alexander Jareno <alexander.jareno@gmail.com>
2015-06-02 17:46:33 +0200

    ny bakåtknapp, ny round progress bar, score-sidan tar in combos bl.a.

--- www/js/score.controller.js
+++ www/js/score.controller.js
@@ -1,12 +1,32 @@
 (function () {
 
-	function scorePageController($ionicActionSheet, $ionicPlatform, $scope){
-		this.points = 0;
-		this.level = 1
-		$scope.roundProgress = {
+	function scorePageController($ionicActionSheet, $ionicPlatform, $scope, $localstorage, $filter){
+		this.max = 500;
+		this.current = 450;
+
+
+		$ionicPlatform.ready(function(){
+			updatePagePoints();
+		});
+
+		function updatePagePoints(){
+			this.points= $localstorage.getPoints();
+			this.level= $filter('levelCheck')(this.points);
+			this.combo= $localstorage.getCombo();
+			console.log(this.points);
+		}
+
+		this.getFontSize = function (){
+
+			if (this.max > 999 || this.current > 999)
+				return "17"+"px";
+		} 
+
+		/*$scope.roundProgress = {
 			label: 100,
 			percentage: 75
 		};
+		*/ //Den gamla progress bar'en
 	}
 
 	angular.module('starter.score.controller', [])
